c74862b5211ccd0e045331f5917803794b90c928
Alia Zat <67161403+ALiaZat@users.noreply.github.com>
2020-12-20 20:02:50 +0200

    last commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,9 @@
 function firstColorChanged() {
     let newColor = document.getElementById("first").value;
-    document.documentElement.style
-        .setProperty('--primary-color', newColor);
+    document.documentElement.style.setProperty('--primary-color', newColor);
 }
 
 function secondColorChanged() {
     let newColor = document.getElementById("second").value;
-    document.documentElement.style
-        .setProperty('--secondary-color', newColor);
+    document.documentElement.style.setProperty('--secondary-color', newColor);
 }
